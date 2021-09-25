0f332633feceffbbae531b055a42116914968127
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2021-09-25 16:51:37 -0300

    Add test for subtraction and equals

---
+++ subtraction_equals.py
@@ -0,0 +1,13 @@
+numOne = int(input('Set number one \n'))
+numTwo = int(input('Set number two \n'))
+sub = int()
+
+if numOne > numTwo:
+	sub = numOne - NumTwo
+	print('The result of subtraction is: %s'% (sub))
+elif numTwo > numOne:
+	sub = numTwo - numOne
+	print('The result of subtraction is: %s'% (sub))
+else:
+	print('The numbers are equals')
+
