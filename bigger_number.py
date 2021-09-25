c58da144241789170b07b5cc31c6646e7a0154ad
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2021-09-25 15:39:11 -0300

    Test of bigger number

---
+++ bigger_number.py
@@ -0,0 +1,10 @@
+a = int(input('Enter number 1'))
+b = int(input('Enter number 2'))
+c = int(input('Enter number 3'))
+
+if a > b and a > c:
+	print('The number one is bigger')
+elif b > a and b > c:
+	print('The number two is bigger')
+elif c > b and c > a:
+	print('The number three is bigger')
