c1c59291939981cd9cf1e9a298a9672ef993fe68
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2021-09-25 16:47:48 -0300

    Add test of substraction

---
+++ subtraction.py
@@ -0,0 +1,9 @@
+numOne = int(input('Set number one \n'))
+numTwo = int(input('Set number two \n'))
+sub = int()
+
+if numOne > numTwo:
+	sub = numOne - NumTwo
+else:
+	sub = numTwo - numOne
+print('The result of subtraction is: %s'% (sub))
