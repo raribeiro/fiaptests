e63d7d557eca336efa56ff5ab7d0f6c6dd1a1907
Rodnei Albuquerque Ribeiro <rodnei.solucoes@gmail.com>
2021-10-05 17:48:10 -0300

    Add Fibonacci validation number

---
+++ ativ_cap3_15_09_a_04_10/RM92676_EXE03.py
@@ -0,0 +1,21 @@
+number = int(input('Informe o número e eu falo se ele faz parte da sequência de Fibonnaci: '))
+startOne = 0
+startTwo = 1
+currentNumber = 0
+successOperation = 0
+
+
+for x in range(number):
+	currentNumber = startOne + startTwo
+	startOne = startTwo
+	startTwo = currentNumber
+	
+	if number == currentNumber:
+		successOperation = 1
+		
+if successOperation == 1:
+	print('Ação foi bem sucedida')
+else:
+	print('Ação foi mal sucedida')
+		
+	
